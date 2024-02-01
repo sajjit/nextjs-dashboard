04eee3eeccb611ab6fcbadaa92caa72db53b54ab
Sajjit Soti <sajjit@192.168.88.252>
2024-02-01 12:55:27 +0545

    Initial

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+ 
+export default function Page() {
+  return (
+    <div>
+      <Image
+        src="/profile.png"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+    </div>
+  )
+}
+
